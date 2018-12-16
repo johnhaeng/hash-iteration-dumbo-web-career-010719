acdf073151cba098724ee2c9759eb4a12c65f83b
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-16 21:22:38 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,7 +10,7 @@ def select_winner(passengers)
   # add the code snippet here!
   winner = ""
 
-  hash.each do |suite, name|
+  passengers.each do |suite, name|
     if suite == :suite_a && name[0] == "A"
       winner = name
     end
